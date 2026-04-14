591d2a9382aa243e6bc56d044158ff81d3611b1c
tangxiand <197554977@qq.com>
2026-04-14 11:22:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# iStore 应用商店
+echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+
+# 设备监控、流量统计、Argon 主题
+echo "src-git nexstore https://github.com/kenzok8/small-package" >> feeds.conf.default
+echo "src-git extra https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default#!/bin/bash
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
