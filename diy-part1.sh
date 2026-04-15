71ba35afa73809708226f1f7b8cc5a68de9a1899
tangxiand <197554977@qq.com>
2026-04-15 21:42:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# 软件源
+# iStore 商店
 echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+
+# 常用插件源
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small-package" >> feeds.conf.default
+
+# RTL8125 2.5G 网卡驱动
 echo "src-git r8125 https://github.com/sbwml/package_kernel_r8125" >> feeds.conf.default
