3ea28c4931ff9bbc9d7ead72248a4ef839d18d57
tangxiand <197554977@qq.com>
2026-04-16 22:09:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# iStore 商店
-echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+# 以下源已经在 feeds.conf.default 里添加，这里注释掉避免重复
+# echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+# echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+# echo "src-git small https://github.com/kenzok8/small-package" >> feeds.conf.default
+# echo "src-git r8125 https://github.com/sbwml/package_kernel_r8125" >> feeds.conf.default
 
-# 常用插件源
-echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small-package" >> feeds.conf.default
+# 清除旧的，防止冲突
+./scripts/feeds clean
 
-# RTL8125 2.5G 网卡驱动
-echo "src-git r8125 https://github.com/sbwml/package_kernel_r8125" >> feeds.conf.default
+# 更新+安装 feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
